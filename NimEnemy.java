d0446d892e456e56e5f7e7ea5739e6edb05ca3e1
GAM3N1GHT <52677583+GAM3N1GHT@users.noreply.github.com>
2025-03-31 22:28:13 -0700

    Beta NimEnemy.java

--- NimEnemy.java
+++ NimEnemy.java
@@ -1,4 +1,71 @@
 public class NimEnemy
 {
-    
+    private int deleteNum = 0;
+    private int integer = 0;
+
+    public void play()
+    {
+        if (NimRunner.enemyTurn == -1)
+        {
+            deleteNum = solve();
+            NimRunner.enemyTurn = 50 + (deleteNum*30);
+        }
+        else if (NimRunner.enemyTurn > 50 && NimRunner.enemyTurn%30 == 0)
+        {
+            if (integer%2 == 1)
+            {
+                MoveableTile.allTiles[integer].changePos(NimRunner.screenWidth-159, (60*((integer-1)/2))+30);
+            }
+            else
+            {
+                MoveableTile.allTiles[integer].changePos(NimRunner.screenWidth-99, (60*((integer/2))+30));
+            }
+            integer++;
+        }
+        else if (NimRunner.enemyTurn == 10)
+        {
+            MoveableTile[] temp = new MoveableTile[MoveableTile.allTiles.length-deleteNum];
+            for (int i = deleteNum; i<MoveableTile.allTiles.length; i++)
+            {
+                temp[i-deleteNum] = MoveableTile.allTiles[i];
+            }
+            MoveableTile.allTiles = temp;
+        }
+        else if (NimRunner.enemyTurn == 0)
+        {
+            deleteNum = 0;
+            integer = 0;
+            NimRunner.nimComp.nextTurn();
+        }
+        NimRunner.enemyTurn--;
+    }
+
+    public int solve()
+    {
+        int numTiles = MoveableTile.allTiles.length;
+        if (numTiles > 3 && numTiles < 7)
+        {
+            return numTiles - 3;
+        }
+        else if (numTiles > 7 && numTiles < 15)
+        {
+            return numTiles - 7;
+        }
+        else if (numTiles > 15 && numTiles < 31)
+        {
+            return numTiles - 15;
+        }
+        else if (numTiles > 31)
+        {
+            return numTiles - 31;
+        }
+        else if (numTiles <= 3)
+        {
+            return 1;
+        }
+        else
+        {
+            return (int)(Math.random()*numTiles/2)+1;
+        }
+    }
 }
